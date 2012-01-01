2712c0983691e776edc1dc880fa41d26fca1097a
ma2799@gmail.com <ma2799@gmail.com@5f9a10a3-f14e-d9e1-4a4f-2b34db622a1a>
2012-01-01 04:46:57 +0000

    Began some serious work. Need to finish getting a Player from a login event

--- RandomBattle/src/me/merdril/RandomBattle/RandomBattle.java
+++ RandomBattle/src/me/merdril/RandomBattle/RandomBattle.java
@@ -6,6 +6,8 @@ package me.merdril.RandomBattle;
 
 import java.util.logging.Logger;
 
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -14,13 +16,17 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class RandomBattle extends JavaPlugin
 {
-	Logger	log	= Logger.getLogger("Minecraft");
+	Logger										log				= Logger.getLogger("Minecraft");
+	private final RandomBattlePlayerListener	playerListener	= new RandomBattlePlayerListener(
+																		this);
 	
 	/**
 	 * @param args
 	 */
 	public void onEnable()
 	{
+		PluginManager pm = this.getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_LOGIN, playerListener, Event.Priority.Monitor, this);
 		log.info("Random Battle has started!");
 	}
 	
