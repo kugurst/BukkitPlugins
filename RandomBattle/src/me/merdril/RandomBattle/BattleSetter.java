1137b6a91df4f2951a74ccb0486762850770084b
ma2799@gmail.com <ma2799@gmail.com@5f9a10a3-f14e-d9e1-4a4f-2b34db622a1a>
2012-01-09 07:31:40 +0000

    I have no effing idea if the stage setter works...

--- RandomBattle/src/me/merdril/RandomBattle/BattleSetter.java
+++ RandomBattle/src/me/merdril/RandomBattle/BattleSetter.java
@@ -1,8 +1,11 @@
 
 package me.merdril.RandomBattle;
 
+import java.util.ArrayList;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.ComplexLivingEntity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
@@ -18,10 +21,15 @@ import org.getspout.spoutapi.player.SpoutPlayer;
  */
 public class BattleSetter
 {
-	RandomBattle	     plugin;
-	protected static int	stageHeight;
-	protected static int	stageWidth;
-	protected static int	stageLength;
+	RandomBattle	                plugin;
+	protected static int	        stageHeight;
+	protected static int	        stageWidth;
+	protected static int	        stageLength;
+	private Location	            startPoint;
+	private Location	            currentPoint;
+	private int	                    side	     = 1;
+	private boolean	                noGoodStage	 = false;
+	private static ArrayList<Block>	editedBlocks	= new ArrayList<Block>();
 	
 	/**
 	 * 
@@ -56,18 +64,23 @@ public class BattleSetter
 		Location playerLocation = player.getLocation();
 		Location monsterLocation = monster.getLocation();
 		// Try to make a stage for the monsters
-		Location startPoint = playerLocation;
+		startPoint = playerLocation;
 		startPoint.setY(stageHeight);
-		startPoint = startPoint.getBlock().getLocation();
-		Location endPoint =
-		        new Location(startPoint.getWorld(), startPoint.getBlockX(), startPoint.getBlockY(),
-		                startPoint.getBlockZ());
-		endPoint = findSafeStage(endPoint);
+		currentPoint = startPoint.getBlock().getLocation();
+		findSafeStage(currentPoint);
+		// Check that a stage was set
+		if (noGoodStage)
+			plugin.getServer()
+			        .getConsoleSender()
+			        .sendMessage(
+			                "[RandomBattle] Failed to set stage for " + player.getDisplayName()
+			                        + " at location " + player.getLocation().toString() + ".");
 		player.sendMessage("[RandomBattle] Stage set.");
 	}
 	
-	private Location findSafeStage(Location startPoint)
+	private void findSafeStage(Location loc)
 	{
+		outer:
 		for (int i = 0; i < stageWidth; i++)
 		{
 			for (int j = 0; j < stageLength; j++)
@@ -75,13 +88,81 @@ public class BattleSetter
 				// 5 for the maximum height of the battle field
 				for (int k = 0; k < 5; k++)
 				{
-					if (endPoint.getBlock().getType().compareTo(Material.AIR) != 0)
-					{}
+					if (!loc.getBlock().getType().equals(Material.AIR))
+					{
+						removeBlocks();
+						if (side == 1)
+						{
+							if (currentPoint.getBlockX() - startPoint.getBlockX() >= stageWidth)
+							{
+								side = 2;
+								currentPoint = startPoint.getBlock().getLocation();
+								findSafeStage(currentPoint);
+								break outer;
+							}
+							currentPoint.add(1, 0, 0);
+							findSafeStage(currentPoint);
+							break outer;
+						}
+						else if (side == 2)
+						{
+							if (startPoint.getBlockX() - currentPoint.getBlockX() >= stageWidth)
+							{
+								side = 3;
+								currentPoint = startPoint.getBlock().getLocation();
+								findSafeStage(currentPoint);
+								break outer;
+							}
+							currentPoint.subtract(1, 0, 0);
+							findSafeStage(currentPoint);
+							break outer;
+						}
+						else if (side == 3)
+						{
+							if (startPoint.getBlockZ() - currentPoint.getBlockZ() >= stageLength)
+							{
+								side = 4;
+								currentPoint = startPoint.getBlock().getLocation();
+								findSafeStage(currentPoint);
+								break outer;
+							}
+							currentPoint.add(0, 0, 1);
+							findSafeStage(currentPoint);
+							break outer;
+						}
+						else if (side == 4)
+						{
+							if (currentPoint.getBlockZ() - startPoint.getBlockZ() >= stageLength)
+							{
+								noGoodStage = true;
+								break outer;
+							}
+							currentPoint.subtract(0, 0, 1);
+							findSafeStage(currentPoint);
+							break outer;
+						}
+					}
 					if (k == 0)
-						endPoint.getBlock().setType(Material.GRASS);
+					{
+						editedBlocks.add(loc.getBlock());
+						loc.getBlock().setType(Material.GRASS);
+					}
+					loc = loc.add(0, 1, 0);
 				}
+				loc.setY(stageHeight);
+				loc.add(0, 0, 1);
 			}
+			loc.setZ(startPoint.getBlockZ() + i + 1);
+			loc.add(1, 0, 0);
 		}
-		return endPoint;
+	}
+	
+	public void removeBlocks()
+	{
+		for (Block block : editedBlocks)
+		{
+			block.setType(Material.AIR);
+			editedBlocks.remove(block);
+		}
 	}
 }
