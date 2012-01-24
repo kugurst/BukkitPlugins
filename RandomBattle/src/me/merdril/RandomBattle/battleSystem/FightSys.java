4e323e9104dbd7c8bad591f78db23c6f47c87015
ma2799@gmail.com <ma2799@gmail.com@5f9a10a3-f14e-d9e1-4a4f-2b34db622a1a>
2012-01-24 20:41:35 +0000

    Need to make the battle system now, along with the AI

---
+++ RandomBattle/src/me/merdril/RandomBattle/battleSystem/FightSys.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package me.merdril.RandomBattle.battleSystem;
+
+/**
+ * @author mark
+ *
+ */
+public class FightSys
+{
+	
+	/**
+	 * 
+	 */
+	public FightSys()
+	{
+		// TODO Auto-generated constructor stub
+	}
+	
+}
