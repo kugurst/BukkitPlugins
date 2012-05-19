8acf8f71e9f4bd9cf25c111d2994d92ad10df310
ma2799@gmail.com <ma2799@gmail.com@5f9a10a3-f14e-d9e1-4a4f-2b34db622a1a>
2012-05-19 17:37:04 +0000

    Renaming to adhere to package name conventions.

---
+++ RandomBattle/src/me/merdril/RandomBattle/hud/MagicButton.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+
+package me.merdril.RandomBattle.hud;
+
+import java.util.ArrayList;
+
+import me.merdril.RandomBattle.RandomBattle;
+
+import org.bukkit.entity.Monster;
+import org.getspout.spoutapi.event.screen.ButtonClickEvent;
+import org.getspout.spoutapi.gui.GenericButton;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+/**
+ * @author Merdril
+ * 
+ */
+public class MagicButton extends GenericButton
+{
+	RandomBattle	plugin;
+	
+	/**
+	 * @param monsters
+	 * 
+	 */
+	public MagicButton(RandomBattle instance, SpoutPlayer player, ArrayList<Monster> monsters)
+	{
+		super();
+		this.plugin = instance;
+		this.setText("Magic");
+	}
+	
+	@Override
+	public void onButtonClick(ButtonClickEvent event)
+	{
+		plugin.getServer().getConsoleSender()
+		        .sendMessage("[RandomBattle] " + this.getText() + " was clicked");
+	}
+	
+}
